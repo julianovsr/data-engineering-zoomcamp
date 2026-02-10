299a96d0ec733ed95fa791174ad6aa4865132e4f
Juliano Ramos <julianovsr@gmail.com>
2026-02-10 02:10:03 +0000

    09-02 Version 2

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,3 +1,7 @@
+import pandas as pd
 import sys
 
-print("Hello from the pipeline script!")    
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
